f9c15c52a6bd5cab6ba8b7593dfa7238fce25caa
tracer-ics2017 <tracer@njuics.org>
2024-03-06 03:04:47 -0800

    >  compile 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  03:04:47 up  1:19,  2 users,  load average: 0.00, 0.00, 0.00 7592336af591948ed9ca5b3fb67ee9d62a32cf9c

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -113,7 +113,7 @@ bool changedWp()
   {
     bool success=1;
     new_val=expr(wp->str,&success);
-    Assert(success,"Watchpoint expression needd to be success.\n");
+   if(success)  printf("Watchpoint expressions are needed to be success.\n");
     now_val=wp->value;
     if(now_val!=new_val)
     {
