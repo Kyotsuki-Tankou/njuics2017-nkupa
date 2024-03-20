c89c38ed49c372df6a0ca758f21fe21883b20daa
2213919-Zhang Zhenjie <2213919@mail.nankai.edu.cn>
2024-03-20 03:18:48 -0700

    modified:   src/monitor/cpu-exec.c 	modified:   src/monitor/debug/expr.c 	modified:   src/monitor/debug/ui.c 	modified:   src/monitor/debug/watchpoint.c 	modified:   src/monitor/monitor.c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,13 +1,18 @@
 #include "nemu.h"
 
+/*
+ NUM HEX AND OR EQ MUT REG ADD SUB DIV LBR RBR:OK
+*/
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
+#include<stdlib.h>
 #include <sys/types.h>
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_EQ,TK_NUM,TK_ADD,TK_SUB,TK_HEX,TK_AND,
+  TK_MUT,TK_DIV,TK_LBR,TK_RBR,TK_REG,TK_OR,TK_DEREF,TK_NEG
 
   /* TODO: Add more token types */
 
@@ -23,8 +28,18 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"\\+", TK_ADD},         // add
+  {"==", TK_EQ},         // equal
+  {"-",TK_SUB},//sub
+  {"\\*",TK_MUT},//multiply
+  {"/",TK_DIV},//divide
+  {"0[Xx][0-9a-fA-F]+",TK_HEX},//hex
+  {"[0-9]+",TK_NUM},//dec
+  {"\\(",TK_LBR},//left breacket
+  {"\\)",TK_RBR},//right bracket
+  {"\\$[a-zA-Z]+",TK_REG},//register
+  {"&&",TK_AND},//AND
+  {"\\|\\|",TK_OR},//OR
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -56,11 +71,17 @@ typedef struct token {
 Token tokens[32];
 int nr_token;
 
+void dbg()
+{
+    printf("The token list is:\n");
+    for(int i=0;i<nr_token;i++)  printf("%d ",tokens[i].type);
+    printf("\n");
+}
 static bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
-
+  int token_count=64;
   nr_token = 0;
 
   while (e[position] != '\0') {
@@ -78,9 +99,25 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+        if(substr_len>=32){
+            printf("This substring lengths is too long(position:%d). \n",position);
+            return false;
+        }
+        if(nr_token>=token_count)
+        {
+            printf("The number of tokens(%d) more than maximum(%d)",nr_token,token_count);
+            return false;
+        }
         switch (rules[i].token_type) {
-          default: TODO();
+        case TK_NOTYPE:break;
+        case TK_NUM:
+        case TK_HEX:
+        case TK_REG:
+            strncpy(tokens[nr_token].str,substr_start,substr_len);
+            tokens[nr_token].str[substr_len]='\0';
+        default: 
+            tokens[nr_token].type=rules[i].token_type;
+            nr_token++;
         }
 
         break;
@@ -95,15 +132,245 @@ static bool make_token(char *e) {
 
   return true;
 }
+/*pa1.3 auxiliary functions*/
+int is_op(int op)
+{
+    int flag=0;
+    switch(op){
+    case TK_ADD:
+    case TK_SUB:
+    case TK_EQ:
+    case TK_OR:
+    case TK_AND:
+    case TK_MUT:
+    case TK_DIV:
+    case TK_DEREF:
+    case TK_NEG:
+        flag=1;
+        break;
+    default:
+        flag=0;
+    }
+    return flag;
+}
+int prio(int op)//priority of token
+{
+    int p;
+    switch(op){
+    case TK_OR:
+        p=1;
+        break;
+    case TK_AND:
+        p=2;
+        break;
+    case TK_EQ:
+        p=3;
+        break;
+    case TK_ADD:
+    case TK_SUB:
+        p=4;
+        break;
 
+    case TK_MUT:
+    case TK_DIV:
+        p=6;
+        break;
+    case TK_NEG:
+        p=8;
+        break;
+    case TK_DEREF:
+        p=7;
+        break;
+    default:
+        p=10;
+        break;
+    }
+    return p;
+}
+int comp(int i,int j)//compare priority between 2 tokens   -1:i<j  0:i==j 1:i>j 2:wrong
+{
+    if(i<0||j<0) return 2;
+    return prio(tokens[i].type)<prio(tokens[j].type)?-1:(prio(tokens[i].type)==prio(tokens[j].type)?0:1);
+}
+int check_part(int p,int q)
+{
+    int top=0,flag=0;
+    for(int i=p;i<=q;i++)
+    {
+        if(tokens[i].type==TK_LBR)  top++,flag++;
+        if(tokens[i].type==TK_RBR)  top--,flag++;
+        if(top<0)  return 1;
+    }
+    if(top!=0)  return 1;
+    return 0;
+}
+int check_parentheses(int p,int q)
+/*
+-1:invalid
+0:valid, but without brackets
+1:valid and with brackets
+*/
+{
+    int flag=check_part(p,q);
+    if(flag)  return -1;
+    if(!(tokens[p].type==TK_LBR&&tokens[q].type==TK_RBR))  return 0;
+    flag=check_part(p+1,q-1);
+    if(flag)  return 0;
+    else  return 1;
+}
+uint32_t exitFailed(bool *success,int p,int q)
+{
+    printf("Wrong expression from %d to %d\n",p,q);
+    dbg();
+    *success=false;
+    return 0;
+}
+uint32_t reg2str(char *str,bool *success)
+{
+    uint32_t val=0;
+    if(strcmp(str,"eax")==0){val=cpu.eax;return val;}
+    if(strcmp(str,"ebx")==0){val=cpu.ebx;return val;}
+    if(strcmp(str,"ecx")==0){val=cpu.ecx;return val;}
+    if(strcmp(str,"edx")==0){val=cpu.edx;return val;}
+    if(strcmp(str,"esp")==0){val=cpu.esp;return val;}
+    if(strcmp(str,"esi")==0){val=cpu.esi;return val;}
+    if(strcmp(str,"ebp")==0){val=cpu.ebp;return val;}
+    if(strcmp(str,"edi")==0){val=cpu.edi;return val;}
+    success=false;
+    return val;
+}
+uint32_t eval(int p,int q,bool *success)
+{
+    // printf("p:%d,q:%d\n",p,q);
+    if(p>q)
+    {
+        return exitFailed(success,p,q);   
+    }  
+    else if(p==q)
+    {
+        int type=tokens[p].type;
+        if(type==TK_NUM||type==TK_HEX){
+            return strtoul(tokens[p].str,NULL,0);
+        }
+        else if(type==TK_REG)
+        {
+            uint32_t val=reg2str(tokens[p].str+1,success);
+            if(*success)  return val;
+            else
+            {
+                   printf("Unknown register %s\n",tokens[p].str);
+                   return 0;
+            }
+            return exitFailed(success,p,q);
+        }
+    }
+    int flag=check_parentheses(p,q);
+    if(flag==-1)  return exitFailed(success,p,q);
+    if(flag==1)  return eval(p+1,q-1,success);
+    uint32_t val1=0,val2=0,val=0;
+    int pos=-1;
+    for(int i=p;i<=q;i++)
+    {
+        int bracket=0;
+        if(bracket==0&&is_op(tokens[i].type))
+        {
+            if(pos==-1)  pos=i;
+            else if(comp(i,pos)<0)  pos=i;
+        }
+        else if(tokens[i].type==TK_LBR)  bracket++;
+        else if(tokens[i].type==TK_RBR)  bracket--;
+    }
+    // printf("pos:%d\n",pos);
+    if(pos==-1)  return exitFailed(success,p,q);
+    // if(tokens[pos].type==TK_NEG)
+    // {
+    //     val=eval(pos+1,q,success);
+    //     val=val*-1;
+    // }  
+    if(tokens[pos].type!=TK_DEREF&&tokens[pos].type!=TK_NEG)  val1=eval(p,pos-1,success);
+    if(*success==0)  return 0;
+    val2=eval(pos+1,q,success);
+    // printf("pos: %d\n",pos);
+    if(*success==0)  return 0;
+    switch(tokens[pos].type)
+    {
+    case TK_ADD:  val=val1+val2;break;
+    case TK_SUB:  val=val1-val2;break;
+    case TK_MUT:  val=val1*val2;break;
+    case TK_DIV:  
+        if(val2==0)
+        {
+            printf("Can't divide by 0\n ");
+            return exitFailed(success,p,q);
+        }  val=val1/val2;break;
+    case TK_AND:  val=val1&&val2;break;
+    case TK_OR:  val=val1||val2;break;
+    case TK_EQ:  val=val1==val2;break;
+    case TK_DEREF:  val=vaddr_read(val2,4);break;
+    case TK_NEG: val=(-1)*val2;break;
+    default:  printf("Unknown type.");return exitFailed(success,p,q);break;
+    }
+	return val;
+}
 uint32_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
+  if (!make_token(e)) { 
     *success = false;
     return 0;
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  int type;
+  for(int i=0;i<nr_token;i++)
+  {
+    type=tokens[i].type;
+    if(type==TK_ADD||type==TK_SUB)
+    {
+        int f=1,flag=i;
+        for(int j=i+1;j<nr_token&&(type==TK_ADD||type==TK_SUB);j++)
+        {
+            flag=j;
+            type=tokens[j].type;
+            if(type==TK_SUB) f*=-1;
+        }
+        // printf("i: %d  flag: %d\n",i,flag);
+        if(flag-i>1)
+        {
+            if(f==1)  tokens[i].type=TK_ADD;
+            else  tokens[i].type=TK_SUB;
+            for(int j=i+1;j+flag-i-1<nr_token;j++)
+            {
+                tokens[j]=tokens[j+(flag-i-1)];
+                // printf("%d ",tokens[j].type);
+            }
+            nr_token-=flag-i-1;
+        }
+    }
+  }
+  //"-"processing
+  for(int i=0;i<nr_token;i++)
+  {
+    if(tokens[i].type==TK_SUB)
+    {
+        if(i==0||tokens[i-1].type==TK_LBR||is_op(tokens[i-1].type))
+        {
+            tokens[i].type=TK_NEG;
+        }
+    }
+  }
+  for(int i=0;i<nr_token;i++)
+  {
+    if(tokens[i].type==TK_MUT&&(i==0||is_op(tokens[i-1].type)))
+    {
+        if(i==0)  tokens[i].type=TK_DEREF;
+        else if(tokens[i-1].type!=TK_DEREF) tokens[i].type=TK_DEREF;
+    }
+  }
+  // for(int i=0;i<nr_token;i++)
+  // {
+  //   printf("%d ",tokens[i].type);
+  // }
+  // printf("\n");
+  uint32_t val;
+  val=eval(0,nr_token-1,success);
+  return val;
 }
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -4,7 +4,7 @@
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
-static WP *head, *free_;
+static WP *head, *free_,*tail;
 
 void init_wp_pool() {
   int i;
@@ -15,9 +15,138 @@ void init_wp_pool() {
   wp_pool[NR_WP - 1].next = NULL;
 
   head = NULL;
+  tail = NULL;
   free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* newWp()
+{
+  if(free_==NULL)
+  {
+    printf("No Memory!\n");
+    return NULL;
+  }
+  WP* p=free_;
+  free_=free_->next;
+  if(head==NULL)
+  {
+    head=p;
+    tail=p;
+  }
+  else
+  {
+    tail->next=p;
+    tail=p;
+  }
+  return tail;
+}
 
+void freeWp(WP *wp)
+{
+  if(head==NULL)
+  {
+    printf("No watchpoint.\n");
+    return;
+  }
+  if(wp==head)
+  {
+    head=head->next;
+    wp->next=free_;
+    free_=wp;
+    return;
+  }
+  WP *tmp=head;
+  while(tmp->next!=wp)  tmp=tmp->next;
+  tmp->next=wp->next;
+  wp->next=free_;
+  free_=wp;
+}
+
+void wpTrav()
+{
+  if(head==NULL)
+  {
+    printf("No any watchpoint.\n");
+    return;
+  }
+  WP *tmp=head;
+  while(tmp!=tail&&tmp!=NULL)
+  {
+    printf("%d %s %u (%#x)\n",tmp->NO,tmp->str,tmp->value,tmp->value);
+    tmp=tmp->next;
+  }
+  printf("%d %s %u (%#x)\n",tail->NO,tail->str,tail->value,tail->value);
+  return;
+}
+
+void delWp(int n)
+{
+  WP *tmp=head;
+  while(tmp!=tail)
+  {
+    if(tmp->NO==n)
+    {
+      freeWp(tmp);
+      printf("Delete success.\n");
+      return;
+    }
+    tmp=tmp->next;
+  }
+  if(tail->NO==n)
+  {
+    freeWp(tail);
+    printf("Delete success.\n");
+    return;
+  }
+  printf("Not found\n");
+  return;
+}
+
+bool changedWp()
+{
+  bool changed=0;
+  WP *wp=head;
+  // bool flags=1;
+  uint32_t now_val,new_val;
+  // printf("%u\n",wp->value);
+  // printf("%s\n",wp->str);
+  // printf("%u\n",expr(wp->str,&flags));
+  while(wp!=tail)
+  {
+    // printf("%u\n",wp->value);
+    // printf("%s\n",wp->str);
+    // printf("%u\n",expr(wp->str,&flags));
+    bool success=1;
+    new_val=expr(wp->str,&success);
+    // printf("%d\n",wp->value);
+    Assert(success,"Watchpoint expressions are needed to be success.\n");
+    now_val=wp->value;
+    if(now_val!=new_val)
+    {
+      printf("Watchpoint %d: %s\n Old value = [%u] changed to the new value = [%u].\n",wp->NO,wp->str,now_val,new_val);
+      changed=true;
+      wp->value=new_val;
+    }
+    wp=wp->next;
+  }
+  if(tail!=NULL)
+  {
+    // printf("%u\n",tail->value);
+    // printf("%s\n",tail->str);
+    // printf("%u\n",expr(tail->str,&flags));
+    bool success=1;
+    new_val=expr(tail->str,&success);
+    // printf("%d\n",tail->value);
+    Assert(success,"Watchpoint expressions are needed to be success.\n");
+    now_val=tail->value;
+    if(now_val!=new_val)
+    {
+      printf("Watchpoint %d: %s\n Old value = [%u-%#08x] changed to the new value = [%u-%#08x].\n",tail->NO,tail->str,now_val,now_val,new_val,new_val);
+      changed=true;
+      tail->value=new_val;
+    }
+  }
+  return changed;
+}
