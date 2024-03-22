af8e2fd5dbc1bacc2eb98f324c3ab67f41d6c9c1
tracer-ics2017 <tracer@njuics.org>
2024-03-22 06:38:12 -0700

    >  compile 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  06:38:12 up  1:09,  3 users,  load average: 0.00, 0.03, 0.00 d481834c6ff188f795853150bc5e1e589c790d4

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -157,8 +157,6 @@ static inline void rtl_pop(rtlreg_t* dest) {
   rtl_lm(dest,&cpu.esp,4);
 }
 
-}
-
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
   *dest=*src1==0?1:0;
