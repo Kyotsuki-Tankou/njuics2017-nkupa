8f693ba2eb86119a90b99359133eda434945e65d
tracer-ics2017 <tracer@njuics.org>
2024-03-22 02:18:57 -0700

    >  compile 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:18:57 up  1:23,  3 users,  load average: 0.00, 0.00, 0.00 3039bbd4257a4ad97571a5373e7cac4a3c8f16f

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -3,10 +3,10 @@
 //arith.c
 make_EHelper(add);
 make_EHelper(sub);
-make_EHelper(cmp);//todo
-make_EHelper(inc);//todo
-make_EHelper(dec);//todo
-make_EHelper(neg);//todo
+make_EHelper(cmp);
+// make_EHelper(inc);//todo
+// make_EHelper(dec);//todo
+// make_EHelper(neg);//todo
 make_EHelper(adc);
 make_EHelper(sbb);
 make_EHelper(mul);
@@ -27,11 +27,11 @@ make_EHelper(call_rm);
 make_EHelper(mov);
 make_EHelper(push);
 make_EHelper(pop);
-make_EHelper(pusha);//todo
-make_EHelper(popa);//todo
-make_EHelper(leave);//todo
-make_EHelper(cltd);//todo
-make_EHelper(cwtl);//todo
+// make_EHelper(pusha);//todo
+// make_EHelper(popa);//todo
+// make_EHelper(leave);//todo
+// make_EHelper(cltd);//todo
+// make_EHelper(cwtl);//todo
 make_EHelper(movsx);
 make_EHelper(movzx);
 make_EHelper(lea);
@@ -40,12 +40,12 @@ make_EHelper(lea);
 make_EHelper(test);//todo
 make_EHelper(and);
 make_EHelper(xor);
-make_EHelper(or);//todo
-make_EHelper(sar);//todo
-make_EHelper(shl);//todo
-make_EHelper(shr);//todo
+// make_EHelper(or);//todo
+// make_EHelper(sar);//todo
+// make_EHelper(shl);//todo
+// make_EHelper(shr);//todo
 make_EHelper(setcc);
-make_EHelper(not);//todo
+// make_EHelper(not);//todo
 
 //prefix.c
 make_EHelper(operand_size);
@@ -56,10 +56,10 @@ make_EHelper(inv);
 make_EHelper(nemu_trap);
 
 //system.c
-make_EHelper(lidt);//todo
-make_EHelper(mov_r2cr);//todo
-make_EHelper(mov_cr2r);//todo
-make_EHelper(int);//todo
-make_EHelper(iret);//todo
-make_EHelper(in);//todo
-make_EHelper(out);//todo
+// make_EHelper(lidt);//todo
+// make_EHelper(mov_r2cr);//todo
+// make_EHelper(mov_cr2r);//todo
+// make_EHelper(int);//todo
+// make_EHelper(iret);//todo
+// make_EHelper(in);//todo
+// make_EHelper(out);//todo
