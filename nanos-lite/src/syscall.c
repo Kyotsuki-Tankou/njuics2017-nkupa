7056ddd3696cb6d95f6366d34d76aa05166688c1
tracer-ics2017 <tracer@njuics.org>
2024-04-18 06:15:37 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  06:15:37 up  8:23,  4 users,  load average: 0.00, 0.00, 0.00 f84f643a355036571555c64ad96ac52ad03b5463

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,13 +9,32 @@ static inline _RegSet* sys_exit(_RegSet *r){
   _halt(SYSCALL_ARG2(r)); 
   return NULL;
 }
+
+uintptr_t sys_write(int fd,void *buf,int len)
+{
+  uintptr_t i=0;
+  if(fd==1||fd==2)
+  {
+    for(;len>0;i++)
+    {
+        _putc(((char*)buf)[i]);
+        len--;
+    }
+  }
+  return i;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
   printf("a[0]=%d SYS_node=%d SYS_exit=%d\n",a[0],SYS_none,SYS_exit);
   switch (a[0]) {
     case SYS_none:return sys_none(r);break;
     case SYS_exit:return sys_exit(r);break;
+    case SYS_write:sys_write((int)a[1],(void*)a[2],(int)a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
