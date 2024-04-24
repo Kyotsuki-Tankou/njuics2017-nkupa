ba0ddcccc8bed59752344d39b049a4cd88bf8c5d
tracer-ics2017 <tracer@njuics.org>
2024-04-24 10:34:53 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  10:34:53 up 16:39,  3 users,  load average: 0.45, 0.56, 0.53 d6264ba9a06f828fc3f65fae4d53b28614b280a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -76,7 +76,7 @@ ssize_t fs_read(int fd,void *buf,size_t len)
 
 ssize_t fs_write(int fd,const void *buf,size_t len)
 {
-    // printf("fd=%d, fs_stdout=%d, fs_size=%d, fs_ssize=%d, screen_height=%d\n",fd,FD_STDOUT,len,file_table[fd].size,_screen.height);
+    printf("fd=%d, fs_stdout=%d, fs_size=%d, fs_ssize=%d, screen_height=%d\n",fd,FD_STDOUT,len,file_table[fd].size,_screen.height);
     ssize_t fs_size=file_table[fd].size;
     if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
     
