4405a61376ba4eac364005fc6fbc22c472d7b0b7
tracer-ics2017 <tracer@njuics.org>
2024-04-24 06:18:01 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  06:18:01 up 12:38,  4 users,  load average: 0.30, 0.36, 0.24 ea1456ad9132820799c89014c74ca51cb9295c98

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -69,7 +69,7 @@ ssize_t fs_read(int fd,void *buf,size_t len)
 
 ssize_t fs_write(int fd,const void *buf,size_t len)
 {
-    printf("fd=%d, fs_stdout=%d",fd,FD_STDOUT);
+    // printf("fd=%d, fs_stdout=%d\n",fd,FD_STDOUT);
     ssize_t fs_size=file_table[fd].size;
     if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
     switch(fd)
