8a05de5739e57a49d3809d036de186017af3e296
2213919-Zhang Zhenjie <2213919@mail.nankai.edu.cn>
2024-04-24 04:51:06 -0700

    modified:   nanos-lite/src/syscall.c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -24,6 +24,42 @@ static inline _RegSet* sys_write(_RegSet *r){
   return NULL;
 }
 
+//int _open(const char *path, int flags, mode_t mode)
+static inline _RegSet* sys_open(_RegSet *r){
+  const char* pathname = (const char*)SYSCALL_ARG2(r);
+  int flag = (int)SYSCALL_ARG3(r);
+  int mode = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_open(pathname,flag,mode);
+  return NULL;
+}
+
+//int _read(int fd, void *buf, size_t count)
+static inline _RegSet* sys_read(_RegSet *r)
+{
+    int fd=(int)SYSCALL_ARG2(r);
+    char *buf=(int)SYSCALL_ARG3(r);
+    size_t count=(int)SYSCALL_ARG4(r);
+    SYSCALL_ARG1(r)=fs_read(fd,buf,count);
+    return NULL;
+}
+
+//int _close(int fd) {
+static inline _RegSet* sys_close(_RegSet *r)
+{
+    int fd=(int)SYSCALL_ARG2(r);
+    SYSCALL_ARG1(r)=fs_close(fd);
+    return NULL;
+}
+
+//off_t _lseek(int fd, off_t offset, int whence)
+static inline _RegSet* sys_lseek(_RegSet *r)
+{
+    int fd=(int)SYSCALL_ARG2(r);
+    off_t offset=(int)SYSCALL_ARG3(r);
+    int whence=(int)SYSCALL_ARG4(r);
+    SYSCALL_ARG1=fs_lseek(fd,offset,whence);
+    return NULL;
+}
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -36,6 +72,10 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:return sys_exit(r);break;
     case SYS_write:return sys_write(r);break;
     case SYS_brk:return 0;
+    case SYS_open:return sys_open(r);break;
+    case SYS_read:return sys_read(r);break;
+    case SYS_close:return sys_close(r);break;
+    case SYS_lseek:return sys_lseek(r);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return NULL;
