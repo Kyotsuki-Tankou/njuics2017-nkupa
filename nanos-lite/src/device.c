660ecb7fabb7d3383d3d154a1bd4aab5e9ce7197
tracer-ics2017 <tracer@njuics.org>
2024-05-09 00:25:31 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  00:25:31 up  6:47,  2 users,  load average: 0.36, 0.26, 0.11 eb854ff3a70e7f999aed97aefacd20c1f662154

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,7 +24,7 @@ size_t events_read(void *buf, size_t len) {
 	}
     else {
 		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
-		Log("Get key: %d %s %s\n", key, keyname[key], down ? "down" : "up");
+		// Log("Get key: %d %s %s\n", key, keyname[key], down ? "down" : "up");
 	}
   // Log("event %d", strlen(buf));
 	return strlen(buf);
