1610b629d9a071d0b4dd432977969604dc5cc301
tracer-ics2017 <tracer@njuics.org>
2024-05-15 05:33:35 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  05:33:35 up 11:10,  2 users,  load average: 0.21, 0.12, 0.06 588e353ed29744919501e0a3e1b84b0f4674ae0f

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -14,9 +14,12 @@ void load_prog(const char *filename) {
 
   uintptr_t entry = loader(&pcb[i].as, filename);
   // TODO: remove the following three lines after you have implemented _umake()
-//   _switch(&pcb[i].as);
-//   current = &pcb[i];
-//   ((void (*)(void))entry)();
+  Log("2");
+  _switch(&pcb[i].as);
+  Log("2");
+  current = &pcb[i];
+  Log("2");
+  ((void (*)(void))entry)();
   _Area stack;
   stack.start = pcb[i].stack;
   stack.end = stack.start + sizeof(pcb[i].stack);
