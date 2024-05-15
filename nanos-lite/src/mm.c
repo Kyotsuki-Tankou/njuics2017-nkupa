52fecebe0ba2a4a9523db962b863856f4bda9e5e
tracer-ics2017 <tracer@njuics.org>
2024-05-15 05:17:19 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  05:17:19 up 10:53,  2 users,  load average: 0.00, 0.00, 0.00 10577fae8976388b516a01acb462fc67cda7c4a9

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -30,7 +30,7 @@ int mm_brk(uint32_t new_brk) {
             }
             current->max_brk=new_brk;
         }
-        current->max_brk=new_brk;
+        current->cur_brk=new_brk;
     }
   return 0;
 }
