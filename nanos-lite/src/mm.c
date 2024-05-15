d9bcffc0bd522653157d8bb56092147f20fe7759
tracer-ics2017 <tracer@njuics.org>
2024-05-15 05:18:53 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  05:18:53 up 10:55,  2 users,  load average: 0.00, 0.00, 0.00 77655100c5e02dd0b4b2a35ae6b7aeed61eba490

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -17,23 +17,41 @@ void free_page(void *p) {
 }
 
 /* The brk() system call handler. */
+// int mm_brk(uint32_t new_brk) {
+//     if(current->cur_brk==0)  current->cur_brk=current->max_brk=new_brk;
+//     else{
+//         if(new_brk>current->max_brk)
+//         {
+//             uintptr_t va=K4(current->max_brk);
+//             while(va<new_brk)
+//             {
+//                 void *pa=new_page();
+//                 _map(&current->as,(void*)va,pa);
+//                 va+=PGSIZE;
+//             }
+//             current->max_brk=new_brk;
+//         }
+//         current->cur_brk=new_brk;
+//     }
+//   return 0;
+// }
 int mm_brk(uint32_t new_brk) {
-    if(current->cur_brk==0)  current->cur_brk=current->max_brk=new_brk;
-    else{
-        if(new_brk>current->max_brk)
-        {
-            uintptr_t va=K4(current->max_brk);
-            while(va<new_brk)
-            {
-                void *pa=new_page();
-                _map(&current->as,(void*)va,pa);
-                va+=PGSIZE;
-            }
-            current->max_brk=new_brk;
-        }
-        current->cur_brk=new_brk;
-    }
-  return 0;
+	if (current->cur_brk == 0) {
+		current->cur_brk = current->max_brk = new_brk;
+	} else {
+		if (new_brk > current->max_brk) {
+	        	// TODO: map memory region [current->max_brk, new_brk)
+	        	// into address space current->as
+			uintptr_t va = K4(current->max_brk);//4k对齐
+	        	while(va < new_brk) {
+                		_map(&current->as, (void *)va, (void *)new_page());
+				va += PGSIZE;
+			}
+			current->max_brk = new_brk; // =va ?
+		}
+	current->cur_brk = new_brk;
+	}	
+	return 0;
 }
 
 void init_mm() {
