b2e8e504a620536f3234a4fb422ebe8c1b7bc9c0
tracer-ics2017 <tracer@njuics.org>
2024-05-15 05:24:41 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  05:24:41 up 11:01,  2 users,  load average: 0.21, 0.15, 0.06 eb2c00ab7924448b6268377262761da0155cc26d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,39 +11,22 @@ ssize_t fs_read(int fd, void *buf, size_t len);
 int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-//     int fd=fs_open(filename,0,0);
-//     Log("fd=%d",fd);
-//     // fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
-//     int size=fs_filesz(fd);
-//     int pnums=size/PGSIZE;
-//     if(!size%PGSIZE)  pnums++;
-//     void *pa=NULL;
-//     void *va=DEFAULT_ENTRY;
-//     for(int i=0;i<pnums;i++)
-//     {
-//         pa=new_page();//申请空闲页
-//         _map(as,va,pa);//物理页->用户程序虚拟地址
-//         fs_read(fd,pa,PGSIZE);//读一页
-//         va+=PGSIZE;
-//     }
-//     fs_close(fd);
-//   return (uintptr_t)DEFAULT_ENTRY;
-int fd = fs_open(filename, 0, 0);
-  int bytes = fs_filesz(fd); //出错在之前为size_t
-
-  Log("Load [%d] %s with size: %d", fd, filename, bytes);
-
-  void *pa,*va = DEFAULT_ENTRY;
-  while(bytes>0){
-  	pa = new_page(); //申请空闲物理页
-  	_map(as, va, pa);//该物理页映射到用户程序虚拟地址空间
-  	fs_read(fd, pa, PGSIZE);  //读一页文件到该物理页
-
-  	va += PGSIZE;
-  	bytes -= PGSIZE;
-    Log("1");
-  }
-  //fs_read(fd,DEFAULT_ENTRY,bytes);
-  fs_close(fd);
+    int fd=fs_open(filename,0,0);
+    Log("fd=%d",fd);
+    // fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
+    int size=fs_filesz(fd);
+    int pnums=size/PGSIZE;
+    if(!size%PGSIZE)  pnums++;
+    void *pa=NULL;
+    void *va=DEFAULT_ENTRY;
+    for(int i=0;i<pnums;i++)
+    {
+        pa=new_page();//申请空闲页
+        _map(as,va,pa);//物理页->用户程序虚拟地址
+        fs_read(fd,pa,PGSIZE);//读一页
+        va+=PGSIZE;
+        Log("1");
+    }
+    fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
