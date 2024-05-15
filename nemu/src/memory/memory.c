2631d93fad144e82ce7a81ecd2fd9783365197fc
tracer-ics2017 <tracer@njuics.org>
2024-05-15 03:41:28 -0700

    >  compile 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  03:41:28 up  9:18,  2 users,  load average: 0.00, 0.02, 0.00 569ecfd0da313aa63b3b30886aa74789854d0035

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -31,49 +31,27 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 #define OFF(va)     ((uint32_t)(va) & 0xfff)
 #define PTE_ADDR(pte)   ((uint32_t)(pte) & ~0xfff)
 
-// paddr_t page_translate(vaddr_t addr,bool flag)
-// {
-//     CR0 cr0=cpu.cr0;
-//     CR3 cr3=cpu.cr3;
-//     if(cr0.paging&&cr0.protect_enable)
-//     {
-//         PDE* pgdirs=(PDE*)PTE_ADDR(cr3.val);
-//         PDE pde=(PDE)paddr_read((uint32_t)(pgdirs+PDX(addr)),4);
-//         assert(pde.present);
-//         PTE* pgtabs=(PTE*)PTE_ADDR(pde.val);
-//         PTE pte=(PTE)paddr_read((uint32_t)(pgtabs+PDX(addr)),4);
-//         assert(pte.present);
-//         pde.accessed=1;
-//         pte.accessed=1;
-//         pte.dirty=flag?1:pte.dirty;
-//         paddr_t paddr=PTE_ADDR(pde.val)|OFF(addr);
-//         return paddr;
-//     }
-//     return addr;
-// }
-paddr_t page_translate(vaddr_t addr, bool w1r0) {
-    //aka page_walk
-    PDE pde, *pgdir;
-    PTE pte, *pgtab;
-    // 只有进入保护模式并开启分页机制后才会进行页级地址转换。。。。。。。。。。
-    if (cpu.cr0.protect_enable && cpu.cr0.paging) {
-	    pgdir = (PDE *)(PTE_ADDR(cpu.cr3.val)); //cr3存放20位的基址作为页目录入口
-	    pde.val = paddr_read((paddr_t)&pgdir[PDX(addr)], 4);
-	    assert(pde.present);
-	    pde.accessed = 1;
-
-	    pgtab = (PTE *)(PTE_ADDR(pde.val));  //页目录存放20位的基址作为页表入口
-	    pte.val = paddr_read((paddr_t)&pgtab[PTX(addr)], 4);
-	    assert(pte.present);
-	    pte.accessed = 1;
-	    pte.dirty = w1r0 ? 1 : pte.dirty; //写则置脏位
-
-	    //pte高20位和线性地址低12位拼接成真实地址
-	    return PTE_ADDR(pte.val) | OFF(addr); 
-	}
-
+paddr_t page_translate(vaddr_t addr,bool flag)
+{
+    CR0 cr0=cpu.cr0;
+    CR3 cr3=cpu.cr3;
+    if(cr0.paging&&cr0.protect_enable)
+    {
+        PDE* pgdirs=(PDE*)PTE_ADDR(cr3.val);
+        PDE pde=(PDE)paddr_read((uint32_t)(pgdirs+PDX(addr)),4);
+        assert(pde.present);
+        PTE* pgtabs=(PTE*)PTE_ADDR(pde.val);
+        PTE pte=(PTE)paddr_read((uint32_t)(pgtabs+PDX(addr)),4);
+        assert(pte.present);
+        pde.accessed=1;
+        pte.accessed=1;
+        pte.dirty=flag?1:pte.dirty;
+        paddr_t paddr=PTE_ADDR(pde.val)|OFF(addr);
+        return paddr;
+    }
     return addr;
 }
+
 // uint32_t vaddr_read(vaddr_t addr, int len) {
 //     if(PTE_ADDR(addr)!=PTE_ADDR(addr+len-1))
 //     {
