6d5269244b2e2662465208ec1d15720e5f672823
tracer-ics2017 <tracer@njuics.org>
2024-05-15 01:35:21 -0700

    >  compile 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:35:21 up  8:08,  3 users,  load average: 0.01, 0.01, 0.00 b35a172b3ccd8f2d53ea913167711f1798524c47

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -58,8 +58,8 @@ make_EHelper(nemu_trap);
 
 //system.c
 make_EHelper(lidt);
-// make_EHelper(mov_r2cr);//todo
-// make_EHelper(mov_cr2r);//todo
+make_EHelper(mov_r2cr);
+make_EHelper(mov_cr2r);
 make_EHelper(int);
 make_EHelper(iret);
 make_EHelper(in);
