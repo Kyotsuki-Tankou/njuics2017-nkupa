8ba8af2f9d0d746c1ee8e300295c096ce8c86f33
tracer-ics2017 <tracer@njuics.org>
2024-05-15 01:38:20 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:38:20 up  8:11,  3 users,  load average: 0.00, 0.00, 0.00 362d25169c91aeaa6f58dd0f42f253268923d06e

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 
   init_fs();
   Log("1");
-  uint32_t entry = loader(NULL, "/bin/pal"); 
+  uint32_t entry = loader(NULL, "/bin/hello"); 
   Log("2");
   Log("entry=%u\n",entry);
   ((void (*)(void))entry)();
