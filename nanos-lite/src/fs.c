89e1db4f0111d917ee49dbef5717a44cdb33e418
tracer-ics2017 <tracer@njuics.org>
2024-05-22 04:59:18 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  04:59:18 up  3:38,  3 users,  load average: 0.03, 0.22, 0.21 6a29a2bfbd4ac70dedfbe63fca3a06f862c1b6b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -74,7 +74,7 @@ ssize_t fs_read(int fd,void *buf,size_t len)
             break;
         case FD_DISPINFO:
             dispinfo_read(buf,file_table[fd].open_offset,len);
-            file_table[fd].open_offset+=len;
+            // file_table[fd].open_offset+=len;
             break;
         default:
             ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
