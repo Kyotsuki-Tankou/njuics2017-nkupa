3cce1f3fa91a3e6638b9ec8df7bb78a344707a73
tracer-ics2017 <tracer@njuics.org>
2024-05-22 04:30:56 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  04:30:56 up  3:10,  3 users,  load average: 0.00, 0.02, 0.01 ed8a43c0a1302dc826dc9db65659c8f5e31342b8

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,6 +26,7 @@ void load_prog(const char *filename) {
 static PCB *current_game = &pcb[0];
 void switch_game() {
   current_game=(current_game==&pcb[0]?&pcb[2]:&pcb[0]);
+  Log("switch!");
 }
 static int freq=0;
 _RegSet* schedule(_RegSet *prev) {
