f0c3c74d9fce5c50889fb00e2aeaeef1144ce4bb
tracer-ics2017 <tracer@njuics.org>
2024-05-22 04:36:19 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  04:36:19 up  3:15,  3 users,  load average: 0.01, 0.02, 0.00 733cd32c847c6be0f040ed6d7c7e6a41b6814f34

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -138,6 +138,8 @@ static void get_display_info() {
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
   fclose(dispinfo);
+  Log("WIDTH:%d\nHEIGHT:%d\n",_screen.width,_screen.height);
+  Log("width:%d\nheight:%d\n",screen_w,screen_h);
   assert(screen_w > 0 && screen_h > 0);
 }
 
