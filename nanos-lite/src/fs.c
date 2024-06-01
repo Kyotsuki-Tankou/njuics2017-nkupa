aec5b8cf3c2d37f71ac283ed58842d5f9acca81a
tracer-ics2017 <tracer@njuics.org>
2024-06-01 00:39:54 -0700

    >  run 2213919 ics Linux ics-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  00:39:54 up  7:09,  4 users,  load average: 0.09, 0.14, 0.26 fd7057de50106894f8c7f7f757666aeda9834f5d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,161 +1,12 @@
-// #include "fs.h"
-
-// typedef struct {
-//   char *name;
-//   size_t size;
-//   off_t disk_offset;
-//   off_t open_offset;
-// } Finfo;
-
-// enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
-
-// /* This is the information about all files in disk. */
-// static Finfo file_table[] __attribute__((used)) = {
-//   {"stdin (note that this is not the actual stdin)", 0, 0},
-//   {"stdout (note that this is not the actual stdout)", 0, 0},
-//   {"stderr (note that this is not the actual stderr)", 0, 0},
-//   [FD_FB] = {"/dev/fb", 0, 0},
-//   [FD_EVENTS] = {"/dev/events", 0, 0},
-//   [FD_DISPINFO] = {"/proc/dispinfo", 128, 0},
-// #include "files.h"
-// };
-
-// #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
-
-// void ramdisk_read(void *buf, off_t offset, size_t len);
-// void ramdisk_write(const void *buf, off_t offset, size_t len);
-// void dispinfo_read(void *buf, off_t offset, size_t len);
-// void fb_write(const void *buf, off_t offset, size_t len);
-// size_t events_read(void *buf, size_t len);
-
-// void init_fs() {
-//   // TODO: initialize the size of /dev/fb
-//   file_table[FD_FB].size=_screen.height*_screen.width*4;
-// //   file_table[FD_FB].size=114514;
-// //   Log("The file size is fit to 114514");
-// }
-
-// size_t fs_filesz(int fd) {
-// 	return file_table[fd].size;
-//     // return 114514;
-// }
-
-// int fs_open(const char *pathname,int flags,int mode)
-// {
-//     Log("Pathname: %s", pathname);
-//     for(int i=0;i<NR_FILES;i++)
-//     {
-//         printf("%s\n",file_table[i].name);
-//         if(strcmp(file_table[i].name,pathname)==0)  
-//         {
-//             Log("File %d",i);
-//             return i;
-//         }
-//     }
-//     Log("File not found");
-//     return -1;
-// }
-
-// ssize_t fs_read(int fd,void *buf,size_t len)
-// {
-//     // ssize_t fs_size=file_table[fd].size;
-//     ssize_t fs_size=fs_filesz(fd);
-//     if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
-//     // printf("fs_read=%d  FD_EVENTS=%d len=%d\n",fd,FD_EVENTS,len);
-//     switch(fd)
-//     {
-//         //case FD_STDIN:
-//         case FD_STDOUT:
-//         case FD_STDIN:
-//         case FD_STDERR:
-//             return 0;
-//         case FD_EVENTS:
-//             len=events_read((void *)buf,len);
-//             break;
-//         case FD_DISPINFO:
-//             dispinfo_read(buf,file_table[fd].open_offset,len);
-//             file_table[fd].open_offset+=len;
-//             break;
-//         default:
-//             ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
-//             file_table[fd].open_offset+=len;
-//             return len;
-//     }
-//     return len;
-// }
-
-// ssize_t fs_write(int fd,const void *buf,size_t len)
-// {
-//     // printf("fd=%d, fs_stdout=%d, fs_size=%d, fs_ssize=%d, screen_height=%d\n",fd,FD_STDOUT,len,file_table[fd].size,_screen.height);
-//     // ssize_t fs_size=file_table[fd].size;
-//     ssize_t fs_size=fs_filesz(fd);
-//     // if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
-    
-//     switch(fd)
-//     {
-//         case FD_STDOUT:
-//         case FD_STDERR:
-//             for(int i=0;i<len;i++)
-//             {
-//                 // Log("%s",((char*)buf)[i]);
-//                 _putc(((char*)buf)[i]);
-//             }
-//             // Log("OUT_END",fd);
-//             return len;
-//         case FD_FB:
-//             fb_write(buf,file_table[fd].open_offset,len);
-//         default:
-//             if(fd!=FD_FB)
-//             {
-//                 if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
-//                 ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
-//             }
-//             file_table[fd].open_offset+=len;
-//             // Log("3");
-//             return len;
-//     }
-//     // Log("3");
-//     return len;
-// }
-
-// off_t fs_lseek(int fd,off_t offset,int whence)
-// {
-//     off_t res=-1;
-//     switch(whence) {
-// 		case SEEK_SET:
-// 			if (offset >= 0 && offset <= file_table[fd].size) {
-// 				file_table[fd].open_offset = offset;
-// 				res = file_table[fd].open_offset;
-// 			}
-// 			break;
-// 		case SEEK_CUR:
-// 			if ((offset + file_table[fd].open_offset >= 0) && (offset + file_table[fd].open_offset <= file_table[fd].size)) {
-// 				file_table[fd].open_offset += offset;
-// 				res = file_table[fd].open_offset;
-// 			}
-// 			break;
-// 		case SEEK_END:
-// 			file_table[fd].open_offset = file_table[fd].size + offset;
-// 			res = file_table[fd].open_offset;
-// 			break;
-// 	}
-// 	return res;
-// }
-
-// int fs_close(int fd)
-// {
-//     return 0;
-// }
 #include "fs.h"
 
 typedef struct {
   char *name;
   size_t size;
-  off_t disk_offset;  //ramdisk中的偏移
-  off_t open_offset;  //目前文件操作的位置 即读写指针
+  off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
-//文件描述符
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
 
 /* This is the information about all files in disk. */
@@ -171,119 +22,128 @@ static Finfo file_table[] __attribute__((used)) = {
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
-extern void ramdisk_read(void *buf, off_t offset, size_t len);
-extern void ramdisk_write(const void *buf, off_t offset, size_t len);
-extern void dispinfo_read(void *buf, off_t offset, size_t len);
-extern void fb_write(const void *buf, off_t offset, size_t len);
-extern size_t events_read(void *buf, size_t len);
+void ramdisk_read(void *buf, off_t offset, size_t len);
+void ramdisk_write(const void *buf, off_t offset, size_t len);
+void dispinfo_read(void *buf, off_t offset, size_t len);
+void fb_write(const void *buf, off_t offset, size_t len);
+size_t events_read(void *buf, size_t len);
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  file_table[FD_FB].size = _screen.height * _screen.width * 4;
+  file_table[FD_FB].size=_screen.height*_screen.width*4;
+//   file_table[FD_FB].size=114514;
+//   Log("The file size is fit to 114514");
 }
 
 size_t fs_filesz(int fd) {
 	return file_table[fd].size;
+    // return 114514;
 }
 
-int fs_open(const char *pathname, int flags, int mode) {
-	//可读写所有文件，故忽略flags mode
-	Log("Pathname: %s", pathname);
-	int i;
-	for (i = 0; i < NR_FILES; i++) {
-		//printf("file name: %s\n", file_table[i].name);
-		if (strcmp(file_table[i].name, pathname) == 0) {
-			return i;
-		}
-	}
-	assert(0);
-	return -1;
+int fs_open(const char *pathname,int flags,int mode)
+{
+    Log("Pathname: %s", pathname);
+    for(int i=0;i<NR_FILES;i++)
+    {
+        printf("%s\n",file_table[i].name);
+        if(strcmp(file_table[i].name,pathname)==0)  
+        {
+            Log("File %d",i);
+            return i;
+        }
+    }
+    Log("File not found");
+    return -1;
 }
 
-ssize_t fs_read(int fd, void *buf, size_t len) {
-	ssize_t fs_size = fs_filesz(fd);
-	//if(file_table[fd].open_offset >= fs_size) //实际上不会出现这情况
-		//return 0;
-	if (file_table[fd].open_offset + len > fs_size) //偏移量不可以超过文件边界 超出部分舍弃
-		len = fs_size - file_table[fd].open_offset;
-	switch(fd) {
-		case FD_STDOUT:
-		case FD_STDERR:
-		case FD_STDIN:
-			return 0;
-		case FD_EVENTS:
-			len = events_read((void *)buf, len);
-			break;
-		case FD_DISPINFO:
-			dispinfo_read(buf, file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;	
-			break;
-		default:
-			ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;
-			break;
-	}
-	return len;
+ssize_t fs_read(int fd,void *buf,size_t len)
+{
+    // ssize_t fs_size=file_table[fd].size;
+    ssize_t fs_size=fs_filesz(fd);
+    if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
+    // printf("fs_read=%d  FD_EVENTS=%d len=%d\n",fd,FD_EVENTS,len);
+    switch(fd)
+    {
+        //case FD_STDIN:
+        case FD_STDOUT:
+        case FD_STDIN:
+        case FD_STDERR:
+            return 0;
+        case FD_EVENTS:
+            len=events_read((void *)buf,len);
+            break;
+        case FD_DISPINFO:
+            dispinfo_read(buf,file_table[fd].open_offset,len);
+            file_table[fd].open_offset+=len;
+            break;
+        default:
+            ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+            file_table[fd].open_offset+=len;
+            return len;
+    }
+    return len;
 }
 
-ssize_t fs_write(int fd, const void *buf, size_t len) {
-	ssize_t fs_size = fs_filesz(fd);
-	switch(fd) {
-		case FD_STDOUT:
-		case FD_STDERR:
-			// call _putc()
-			// 串口已被抽象成stdout stderr
-			for(int i = 0; i < len; i++) {
-				_putc(((char*)buf)[i]);
-			}
-			break;
-		case FD_FB:
-			// write to frame buffer 显存
-			// device.c:fb_write buff中len字节输出到屏幕上offest处
-			fb_write(buf, file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;
-			break;
-		default:
-			// write to ramdisk
-			//if(file_table[fd].open_offset >= fs_size)
-				//return 0;	
-			if(file_table[fd].open_offset + len > fs_size)
-				len = fs_size - file_table[fd].open_offset;
-			// 对文件的真正读写
-			ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-			file_table[fd].open_offset += len;
-			//Log("offset = %d", file_table[fd].open_offset);
-			break;
-	}
-	return len;// 参见man 返回值
+ssize_t fs_write(int fd,const void *buf,size_t len)
+{
+    // printf("fd=%d, fs_stdout=%d, fs_size=%d, fs_ssize=%d, screen_height=%d\n",fd,FD_STDOUT,len,file_table[fd].size,_screen.height);
+    // ssize_t fs_size=file_table[fd].size;
+    ssize_t fs_size=fs_filesz(fd);
+    // if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
+    
+    switch(fd)
+    {
+        case FD_STDOUT:
+        case FD_STDERR:
+            for(int i=0;i<len;i++)
+            {
+                // Log("%s",((char*)buf)[i]);
+                _putc(((char*)buf)[i]);
+            }
+            // Log("OUT_END",fd);
+            return len;
+        case FD_FB:
+            fb_write(buf,file_table[fd].open_offset,len);
+        default:
+            if(fd!=FD_FB)
+            {
+                if(file_table[fd].open_offset+len>fs_size)  len=fs_size-file_table[fd].open_offset;
+                ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
+            }
+            file_table[fd].open_offset+=len;
+            // Log("3");
+            return len;
+    }
+    // Log("3");
+    return len;
 }
 
-off_t fs_lseek(int fd, off_t offset, int whence) {
-	off_t result = -1;
-	// fs.h
-	// man 2 lseek 同时注意边界问题
-	switch(whence) {
+off_t fs_lseek(int fd,off_t offset,int whence)
+{
+    off_t res=-1;
+    ssize_t fs_size=fs_filesz(fd);
+    switch(whence) {
 		case SEEK_SET:
-			if (offset >= 0 && offset <= file_table[fd].size) {
+			if (offset >= 0 && offset <= fs_size) {
 				file_table[fd].open_offset = offset;
-				result = file_table[fd].open_offset;
+				res = file_table[fd].open_offset;
 			}
 			break;
 		case SEEK_CUR:
-			if ((offset + file_table[fd].open_offset >= 0) && (offset + file_table[fd].open_offset <= file_table[fd].size)) {
+			if ((offset + file_table[fd].open_offset >= 0) && (offset + file_table[fd].open_offset <= fs_size)) {
 				file_table[fd].open_offset += offset;
-				result = file_table[fd].open_offset;
+				res = file_table[fd].open_offset;
 			}
 			break;
 		case SEEK_END:
-			file_table[fd].open_offset = file_table[fd].size + offset;
-			result = file_table[fd].open_offset;
+			file_table[fd].open_offset = fs_size + offset;
+			res = file_table[fd].open_offset;
 			break;
 	}
-	
-	return result;
+	return res;
 }
 
-int fs_close(int fd) {
-	return 0;
+int fs_close(int fd)
+{
+    return 0;
 }
